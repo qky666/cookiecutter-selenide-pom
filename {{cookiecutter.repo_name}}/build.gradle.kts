b00d4e7dc68aa4c37070897fa9d05df609ad6e6a
qky <qky669@gmail.com>
2023-06-05 14:21:58 +0200

    build.gradle.kts fix

--- {{cookiecutter.repo_name}}/build.gradle.kts
+++ {{cookiecutter.repo_name}}/build.gradle.kts
@@ -19,12 +19,10 @@ dependencies {
     val log4jVersion = "2.20.0"
 
     testImplementation("com.github.qky666:selenide-pom:{{ cookiecutter.selenide_pom_version }}")
-
+    {%- print("\n    ") -%}
     {%- if cookiecutter.use_appium == "yes" -%}
-    \n
     testImplementation("com.codeborne:selenide-appium:{{ cookiecutter.selenide_version }}")
     {%- else -%}
-    \n
     testImplementation("com.codeborne:selenide:{{ cookiecutter.selenide_version }}")
     {% endif %}
     testImplementation("org.testng:testng:7.8.0")
